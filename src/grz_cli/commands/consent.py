67fee0f55840419b8021e74a65c51d125aae4d31
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-04-29 15:17:28 +0200

    feat: add `consent` command

---
+++ src/grz_cli/commands/consent.py
@@ -0,0 +1,65 @@
+"""Command for determining whether a submission is consented for research."""
+
+import json
+import logging
+from pathlib import Path
+
+import click
+
+from ..workers.submission import SubmissionMetadata
+from .common import config_file, submission_dir
+
+log = logging.getLogger(__name__)
+
+mdat_wissenschaftlich_nutzen_eu_dsgvo_niveau = "2.16.840.1.113883.3.1937.777.24.5.3.8"
+FHIR_PROVISION_PERMIT = "permit"
+FHIR_PROVISION_DENY = "deny"
+
+
+@click.command()
+@submission_dir
+@config_file
+def consent(submission_dir):
+    """
+    Check if a submission is consented for research.
+
+    Returns 'true' if consented, 'false' if not.
+    A submission is considered consented if all donors have consented for research, that is
+    the FHIR MII IG Consent profiles all have a "permit" provision for code 2.16.840.1.113883.3.1937.777.24.5.3.8
+    """
+    metadata = SubmissionMetadata(Path(submission_dir) / "metadata" / "metadata.json").content
+
+    consents = {donor.donor_pseudonym: False for donor in metadata.donors}
+    for donor in metadata.donors:
+        for research_consent in donor.research_consents:
+            # TODO rename scope
+            mii_consent = research_consent.scope
+            if isinstance(mii_consent, str):
+                mii_consent = json.loads(mii_consent)
+            if top_level_provision := mii_consent.get("provision"):
+                if not top_level_provision.get("type") == FHIR_PROVISION_DENY:
+                    raise ValueError(
+                        f"Top level provision type must be deny, not {top_level_provision.get('type')}. "
+                        f"(Opt-in via nested provisions)"
+                    )
+                else:
+                    nested_provisions = top_level_provision.get("provision")
+                    consents[donor.donor_pseudonym] = _check_nested_provisions(nested_provisions)
+            else:
+                consents[donor.donor_pseudonym] = False
+
+    if all(consents.values()):
+        click.echo("true")
+    else:
+        click.echo("false")
+
+
+def _check_nested_provisions(provisions):
+    for provision in provisions:
+        if provision.get("type") == FHIR_PROVISION_PERMIT:
+            for code in provision.get("codes"):
+                # TODO check system code as well
+                # TODO check permit period start and end as well
+                if code == mdat_wissenschaftlich_nutzen_eu_dsgvo_niveau:
+                    return True
+    return False
