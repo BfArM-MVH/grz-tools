5e668a2665e3d79662f8907617e1cf1bed65b292
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-04-29 09:56:37 +0200

    skip explicit __all__

--- src/grz_cli/utils/__init__.py
+++ src/grz_cli/utils/__init__.py
@@ -1,16 +1,8 @@
 """Utility functions and classes for the GRZ CLI."""
 
+# ruff: noqa: F401
 from .checksums import calculate_sha256
 from .config import read_config
 from .crypt import Crypt4GH
 from .io import TqdmIOWrapper, read_multiple_json
 from .paths import is_relative_subdirectory
-
-__all__ = [
-    "Crypt4GH",
-    "TqdmIOWrapper",
-    "calculate_sha256",
-    "is_relative_subdirectory",
-    "read_config",
-    "read_multiple_json",
-]
--- src/grz_cli/validation/__init__.py
+++ src/grz_cli/validation/__init__.py
@@ -1,6 +1,5 @@
 """Validation package for grz-cli."""
 
+# ruff: noqa: F401
 from .bam import validate_bam
 from .fastq import validate_paired_end_reads, validate_single_end_reads
-
-__all__ = ["validate_bam", "validate_paired_end_reads", "validate_single_end_reads"]
