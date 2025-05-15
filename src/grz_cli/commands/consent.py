031448ecd1300dac00e4ae220f536f5ffeebeb07
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-05-15 11:51:48 +0200

    simplify table + typing

--- src/grz_cli/commands/consent.py
+++ src/grz_cli/commands/consent.py
@@ -4,13 +4,14 @@ import json
 import logging
 import sys
 from pathlib import Path
+from typing import Any
 
 import click
 import rich.console
 import rich.table
 import rich.text
 
-from ..workers.submission import SubmissionMetadata
+from ..workers.submission import GrzSubmissionMetadata, SubmissionMetadata
 from .common import output_json, show_details, submission_dir
 
 log = logging.getLogger(__name__)
@@ -47,26 +48,24 @@ def consent(submission_dir, output_json, show_details):
         _print_rich_table(consents)
 
 
-def _print_rich_table(consents):
+def _print_rich_table(consents: dict[str, bool]):
     console = rich.console.Console()
     table = rich.table.Table()
     table.add_column("Donor", no_wrap=True)
-    table.add_column("Consent Type", no_wrap=True)
-    table.add_column("Consent State", no_wrap=True)
+    table.add_column("Research Consent", no_wrap=True)
     for donor_pseudonym, consent_value in consents.items():
-        status_text = rich.text.Text(
+        research_consent = rich.text.Text(
             "True" if consent_value else "False",
             style="green" if consent_value else "red",
         )
         table.add_row(
             donor_pseudonym,
-            "research consent",
-            status_text,
+            research_consent,
         )
     console.print(table)
 
 
-def _gather_consent_information(metadata):
+def _gather_consent_information(metadata: GrzSubmissionMetadata) -> dict[str, bool]:
     consents = {donor.donor_pseudonym: False for donor in metadata.donors}
     for donor in metadata.donors:
         for research_consent in donor.research_consents:
@@ -90,13 +89,13 @@ def _gather_consent_information(metadata):
     return consents
 
 
-def _check_nested_provisions(provisions):
+def _check_nested_provisions(provisions: list[dict[str, Any]]) -> bool:
     for provision in provisions:
         if provision.get("type") == FHIR_PROVISION_PERMIT:
-            for code in provision.get("code"):
+            for code in provision.get("code", []):
                 # TODO check system code as well
                 # TODO check permit period start and end as well
-                for coding in code.get("coding"):
+                for coding in code.get("coding", []):
                     code_ = coding.get("code")
                     if code_ == MDAT_WISSENSCHAFTLICH_NUTZEN_EU_DSGVO_NIVEAU:
                         return True
