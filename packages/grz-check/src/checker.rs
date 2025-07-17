018a6fb06cb9928fc721523cb1d325842adf2cb4
Till Hartmann <till.hartmann@bih-charite.de>
2025-07-17 11:48:40 +0200

    fix width of throughput information

--- packages/grz-check/src/checker.rs
+++ packages/grz-check/src/checker.rs
@@ -240,7 +240,7 @@ pub fn run_check(
     }
 
     let file_style = ProgressStyle::with_template(
-        "{prefix:15.bold} [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta}) {wide_msg}",
+        "{prefix:15.bold} [{bar:40.cyan/blue}] {bytes:>10}/{total_bytes:<10} ({bytes_per_sec:>15}, ETA: {eta:>8}) {wide_msg}"
     )?.progress_chars("#>-");
     let main_pb = mpb.add(ProgressBar::new(total_bytes));
     main_pb.set_style(file_style.clone());
