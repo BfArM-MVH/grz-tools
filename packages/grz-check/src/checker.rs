8924dc3cfb636e516782c3092347936941790868
Till Hartmann <till.hartmann@bih-charite.de>
2025-07-17 11:49:20 +0200

    change progress_chars to unicode blocks

--- packages/grz-check/src/checker.rs
+++ packages/grz-check/src/checker.rs
@@ -241,7 +241,7 @@ pub fn run_check(
 
     let file_style = ProgressStyle::with_template(
         "{prefix:15.bold} [{bar:40.cyan/blue}] {bytes:>10}/{total_bytes:<10} ({bytes_per_sec:>15}, ETA: {eta:>8}) {wide_msg}"
-    )?.progress_chars("#>-");
+    )?.progress_chars("█▒░");
     let main_pb = mpb.add(ProgressBar::new(total_bytes));
     main_pb.set_style(file_style.clone());
     main_pb.set_prefix("Overall");
