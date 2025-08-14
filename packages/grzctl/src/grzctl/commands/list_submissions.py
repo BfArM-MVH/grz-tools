770e1f9eced97e7e610e216cd6ce14a1a4782fe3
Travis Wrightsman <travis.wrightsman@uni-tuebingen.de>
2025-08-14 08:27:35 +0000

    add state to JSON + fixes

--- packages/grzctl/src/grzctl/commands/list_submissions.py
+++ packages/grzctl/src/grzctl/commands/list_submissions.py
@@ -11,7 +11,7 @@ import rich.table
 import rich.text
 from grz_common.cli import config_file, output_json
 from grz_common.workers.download import InboxSubmissionState, InboxSubmissionSummary, query_submissions
-from grz_db.models.submission import SubmissionDb, SubmissionStateEnum
+from grz_db.models.submission import SubmissionDb
 from pydantic_core import to_jsonable_python
 
 from ..models.config import ListConfig
@@ -20,39 +20,43 @@ from .db import get_submission_db_instance
 log = logging.getLogger(__name__)
 
 
-def _get_latest_state_txt(submission_db: SubmissionDb, submission_id: str) -> rich.text.Text:
+def _get_latest_state_str(submission_db: SubmissionDb, submission_id: str) -> str | None:
+    submission_from_db = submission_db.get_submission(submission_id)
+    if submission_from_db:
+        latest_state_log = submission_from_db.get_latest_state()
+        latest_state = None if latest_state_log is None else latest_state_log.state.value
+    else:
+        latest_state = "missing"
+
+    return latest_state
+
+
+def _get_latest_state_txt(latest_state: str | None) -> rich.text.Text:
     """
     Gets the latest database state of a submission ID as a Rich Text object,
     while also handling missing submissions and submissions without a state yet.
     """
-    submission_from_db = submission_db.get_submission(submission_id)
-    if submission_from_db:
-        latest_state = submission_from_db.get_latest_state()
-        if latest_state is None:
-            latest_state_txt = rich.text.Text("none", style="italic yellow")
-        else:
-            latest_state_str = latest_state.state.value
-            latest_state_txt = (
-                rich.text.Text(latest_state_str, style="red")
-                if latest_state_str == SubmissionStateEnum.ERROR
-                else rich.text.Text(latest_state_str)
-            )
-    else:
+    if latest_state is None:
+        latest_state_txt = rich.text.Text("none", style="italic yellow")
+    elif latest_state == "missing":
         latest_state_txt = rich.text.Text("missing", style="italic yellow")
+    else:
+        latest_state_txt = (
+            rich.text.Text(latest_state, style="red") if latest_state == "Error" else rich.text.Text(latest_state)
+        )
 
     return latest_state_txt
 
 
-def _prepare_table(summaries: list[InboxSubmissionSummary], config: ListConfig) -> rich.table.Table:
+def _prepare_table(summaries: list[InboxSubmissionSummary], database_states: dict[str, str | None]) -> rich.table.Table:
     """
     Constructs a nice Rich Table to display inbox status and database state of submissions in the inbox.
     """
     table = rich.table.Table()
     table.add_column("ID", no_wrap=True)
     table.add_column("Inbox Status", no_wrap=True, justify="center")
-    if config.db is not None:
+    if database_states:
         table.add_column("Database State", no_wrap=True, justify="center", style="green")
-        submission_db = get_submission_db_instance(db_url=config.db.database_url)
     table.add_column("Oldest Upload", overflow="fold")
     table.add_column("Newest Upload", overflow="fold")
     for summary in summaries:
@@ -75,25 +79,18 @@ def _prepare_table(summaries: list[InboxSubmissionSummary], config: ListConfig) 
             summary.oldest_upload.astimezone().strftime("%Y-%m-%d %H:%M:%S"),
             summary.newest_upload.astimezone().strftime("%Y-%m-%d %H:%M:%S"),
         ]
-        if submission_db is not None:
-            latest_state_txt = _get_latest_state_txt(submission_db, summary.submission_id)
+        if database_states:
+            latest_state_txt = _get_latest_state_txt(database_states[summary.submission_id])
             row.insert(2, latest_state_txt)
         table.add_row(*row)
     return table
-
-
-def _validate_limit(ctx: click.Context, param: click.Parameter, value: int):
-    if value < 0:
-        raise click.BadParameter("limit must be a positive integer")
-
-    return value
 
 
 @click.command()
 @config_file
 @output_json
 @click.option("--show-cleaned/--hide-cleaned", help="Show cleaned submissions.")
-@click.option("--limit", type=int, default=10, callback=_validate_limit)
+@click.option("--limit", type=click.IntRange(min=0), default=10)
 def list_submissions(config_file: Path, output_json: bool, show_cleaned: bool, limit: int):
     """
     List submissions within an inbox from oldest to newest, up to the requested limit.
@@ -101,11 +98,22 @@ def list_submissions(config_file: Path, output_json: bool, show_cleaned: bool, l
     config = ListConfig.from_path(config_file)
     submissions = query_submissions(config.s3, show_cleaned)
 
+    database_states: dict[str, str | None] = {}
+    if config.db is not None:
+        submission_db = get_submission_db_instance(db_url=config.db.database_url)
+        # query latest database state for submissions
+        for submission in submissions:
+            database_states[submission.submission_id] = _get_latest_state_str(submission_db, submission.submission_id)
+
     if output_json:
-        json.dump(to_jsonable_python(submissions), sys.stdout)
+        submissions_jsonable = to_jsonable_python(submissions[:limit])
+        if database_states:
+            for submission in submissions_jsonable:
+                submission["database_state"] = database_states[submission["submission_id"]]
+        json.dump(submissions_jsonable, sys.stdout)
     else:
         console = rich.console.Console()
-        table = _prepare_table(submissions[:limit], config)
+        table = _prepare_table(submissions[:limit], database_states)
         if len(submissions) > limit:
             console.print(f"[yellow]Limiting display to {limit} out of {len(submissions)} total submissions.[/yellow]")
         console.print(table)
