5099746871fcc00f8b5f0c0c407cfbd3eb74b450
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-09-30 14:36:52 +0200

    fix multistage docker build (hopefully)

--- packages/grz-watchdog/build.sh
+++ packages/grz-watchdog/build.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-# configuration
-# final, usable image for grz-watchdog workflow
 GRZ_WATCHDOG_WORKFLOW_IMAGE="grz-watchdog:latest"
-# temporary image, contains conda envs and labels from snakemake
-SNAKEMAKE_CONTAINERIZED_ENVS_IMAGE="grz-watchdog-conda-envs:latest"
 
-# generate dockerfile from snakemake
 snakemake --containerize > Dockerfile.snake.tmp
 
-# check if an old Dockerfile exists and if it's identical to the new one
 if [ -f Dockerfile.snake ] && cmp -s Dockerfile.snake Dockerfile.snake.tmp; then
   rm Dockerfile.snake.tmp
 else
   mv Dockerfile.snake.tmp Dockerfile.snake
 fi
 
+sed -i '1s/$/ AS conda-base/' Dockerfile.snake
 
+sed -e '/# --- SNAKEMAKE-CONTAINERIZE-CONTENT-GOES-HERE ---/r Dockerfile.snake' \
+    -e '/# --- SNAKEMAKE-CONTAINERIZE-CONTENT-GOES-HERE ---/d' \
+    Dockerfile.template > Dockerfile
 
-# build intermediate image
-podman build -t "${SNAKEMAKE_CONTAINERIZED_ENVS_IMAGE}" -f Dockerfile.snake .
-
-# build final image, inherits from intermediate image
-# pass intermediate image name as build-arg
-podman build \
-	--build-arg SNAKEMAKE_BASE_IMAGE="${SNAKEMAKE_CONTAINERIZED_ENVS_IMAGE}" \
-	-t "${GRZ_WATCHDOG_WORKFLOW_IMAGE}" \
-	-f Dockerfile .
-
-# clean up intermediate artifacts
-podman rmi "${SNAKEMAKE_CONTAINERIZED_ENVS_IMAGE}"
+podman build -t "${GRZ_WATCHDOG_WORKFLOW_IMAGE}" -f Dockerfile .
