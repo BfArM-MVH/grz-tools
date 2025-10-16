a7411e6b70e048738bb7ae8205c0cc5513f449f3
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-10-16 11:42:19 +0200

    fix(grzctl): fix quarter determination logic

--- packages/grzctl/tests/cli/test_report.py
+++ packages/grzctl/tests/cli/test_report.py
@@ -16,6 +16,28 @@ from grzctl.models.config import DbConfig
 from .. import resources as test_resources
 
 
+def test_quarter_determination():
+    """Test that quarter determination works as expected."""
+    dates = [
+        ((1, 2025), date.fromisoformat("2025-01-01")),
+        ((1, 2025), date.fromisoformat("2025-02-01")),
+        ((1, 2025), date.fromisoformat("2025-03-01")),
+        ((2, 2025), date.fromisoformat("2025-04-01")),
+        ((2, 2025), date.fromisoformat("2025-05-01")),
+        ((2, 2025), date.fromisoformat("2025-06-01")),
+        ((3, 2025), date.fromisoformat("2025-07-01")),
+        ((3, 2025), date.fromisoformat("2025-08-01")),
+        ((3, 2025), date.fromisoformat("2025-09-01")),
+        ((4, 2025), date.fromisoformat("2025-10-01")),
+        ((4, 2025), date.fromisoformat("2025-11-01")),
+        ((4, 2025), date.fromisoformat("2025-12-01")),
+    ]
+    for (expected_quarter, expected_year), check_date in dates:
+        quarter, year = date_to_quarter_year(check_date)
+        assert quarter == expected_quarter
+        assert year == expected_year
+
+
 def test_quarterly_empty(blank_database_config_path: Path, tmp_path: Path):
     """Quarterly reports should work on an empty database."""
     env = {
