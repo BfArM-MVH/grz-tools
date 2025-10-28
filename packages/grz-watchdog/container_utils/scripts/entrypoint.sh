0a0308640d9efbd6b81e16f6c9233af2531a807b
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-10-28 16:26:44 +0100

    update containter util scripts / entrypoint

--- packages/grz-watchdog/container_utils/scripts/entrypoint.sh
+++ packages/grz-watchdog/container_utils/scripts/entrypoint.sh
@@ -6,5 +6,4 @@ pixi run --manifest-path /grz-watchdog/pixi.toml -- \
     --workflow-profile /grz-watchdog/workflow/profiles/default \
     --snakefile /grz-watchdog/workflow/Snakefile \
     --directory /workdir \
-    --conda-prefix /conda-envs \
     "$@"
