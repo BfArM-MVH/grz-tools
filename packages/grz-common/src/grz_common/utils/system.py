6f7095f3de6acdc39f011d7f4376fac2b221651f
Virag Sharma <viragsharma@w620-mb006-wl.speedport.ip>
2026-02-13 15:02:45 +0100

    added get_cpu_count function

--- packages/grz-common/src/grz_common/utils/system.py
+++ packages/grz-common/src/grz_common/utils/system.py
@@ -9,7 +9,10 @@ else:
         cpu_count = os.cpu_count() or 1
         return set(range(cpu_count))
 
-
 def get_cpu_affinity(pid: int = 0) -> set[int]:
     """Return the set of CPUs available to the process (cross-platform safe)."""
     return _sched_getaffinity(pid)
+
+def get_cpu_count(pid: int = 0) -> int:
+    """Return the number of CPUs available to the process."""
+    return len(get_cpu_affinity(pid))
