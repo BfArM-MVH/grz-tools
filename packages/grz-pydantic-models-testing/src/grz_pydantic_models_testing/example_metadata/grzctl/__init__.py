bcaf514573aef8c903b7cf37d04b7a8d93f46122
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2026-04-24 16:19:20 +0200

    make all test resources valid python modules

--- packages/grz-pydantic-models-testing/src/grz_pydantic_models_testing/example_metadata/grzctl/__init__.py
+++
@@ -1,5 +0,0 @@
-# Subpackage exposing grzctl-specific example metadata fixtures.
-# Usage:
-#   from grz_pydantic_models_testing.example_metadata import grzctl as grzctl_metadata
-#   import importlib.resources
-#   path = importlib.resources.files(grzctl_metadata).joinpath("metadata.json")
